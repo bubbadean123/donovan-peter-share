f2d6adcdb8c17a32577aba4c6c8be585d5739fa8
Donovan Lampa <donovan.lampa@gmail.com>
2015-10-18 14:28:29 -0500

    Try new write file.

--- comp.rb
+++ comp.rb
@@ -1,3 +1,6 @@
+require 'tempfile'
+require 'fileutils'
+
 $tape=nil
 
 def tape_read(char)
@@ -5,19 +8,35 @@ def tape_read(char)
   return $tape.read.split("\n")[char]
 end
 
+# def tape_write(char,data)
+#   tcont=$tape.read.split("\n")
+#   puts tcont.inspect
+#   if tcont[char]==nil
+#     tcont.unshift(data)
+#   else
+#     tcont[char]=data
+#   end
+#   puts tcont.inspect
+#   tcont=tcont.join("\n")
+#   puts "About to write string #{tcont}"
+#   $tape.rewind
+#   $tape.puts(tcont)
+# end
+
 def tape_write(char,data)
-  tcont=$tape.read.split("\n")
-  puts tcont.inspect
-  if tcont[char]==nil
-    tcont.unshift(data)
+  current_content_array = $tape.read.split("\n")
+  new_file = Tempfile.new("temp")
+  if current_content_array[char].nil?
+    current_content_array << data
   else
-    tcont[char]=data
+    current_content_array[char] = data
   end
-  puts tcont.inspect
-  tcont=tcont.join("\n")
-  puts "About to write string #{tcont}"
-  $tape.rewind
-  $tape.puts(tcont)
+  current_content_array.each do |character|
+    new_file.puts(character)
+  end
+  new_file.close
+  old_path = $tape.path
+  FileUtils.mv(new_file.path, old_path)
 end
 
 def tape_insert(name)
