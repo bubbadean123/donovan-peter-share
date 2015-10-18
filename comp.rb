0ec4a40f5c166555e77de6b68e15b80f319d9194
Donovan Lampa <donovan.lampa@gmail.com>
2015-10-18 14:19:34 -0500

    Add some formatting and debug messages

--- comp.rb
+++ comp.rb
@@ -1,31 +1,38 @@
 $tape=nil
+
 def tape_read(char)
-$tape.rewind()
-return $tape.read.split("\n")[char]
+  $tape.rewind()
+  return $tape.read.split("\n")[char]
 end
+
 def tape_write(char,data)
-tcont=$tape.read.split("\n")
-if tcont[char]==nil
-tcont.unshift(data)
-else
-tcont[char]=data
+  tcont=$tape.read.split("\n")
+  puts tcont.inspect
+  if tcont[char]==nil
+    tcont.unshift(data)
+  else
+    tcont[char]=data
+  end
+  puts tcont.inspect
+  tcont=tcont.join("\n")
+  puts "About to write string #{tcont}"
+  $tape.rewind
+  $tape.puts(tcont)
 end
-puts tcont.inspect
-tcont=tcont.join("\n")
-$tape.rewind()
-$tape.puts(tcont)
-end
+
 def tape_insert(name)
-if File.exists?(name+".tap")
-$tape = File.open(name+".tap","a+")
-else
-$tape = File.open(name+".tap","w+")
+  if File.exists?(name+".tap")
+    $tape = File.open(name+".tap","a+")
+  else
+    $tape = File.open(name+".tap","w+")
+  end
 end
-end
+
 def tape_eject()
-$tape.close()
-$tape=nil
+  $tape.close()
+  $tape=nil
 end
+
 i=0
 tape_insert("test")
 tape_write(i,"a")
