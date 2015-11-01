4bd1fa7d4a58e70e1076cb58260fabf0bfaf1489
pjht <redterp@hotmail.com>
2015-11-01 13:10:10 -0600

    Now in class

--- comp.rb
+++ comp.rb
@@ -1,49 +1,54 @@
-$tape=nil
+class Tape
+#reading endc:endc()
+#writing endc:endc(true,data)
+def initialize(end_char="~")
+@tape=""
+@endc
+end
 
 def tape_read(char)
-  $tape.rewind()
-  return $tape.read.split("\n")[char]
+@tape.rewind()
+return @tape.read.split("\n")[char]
 end
 
 def tape_write(char,data,content=[])
-  $tape.rewind
-  #How do we fix this?
-  if content.empty?
-    current_content_array = $tape.read.split("\n")
-  else
-    current_content_array = content
-  end
-  $tape.rewind
-  current_content_array[char] = data
-  current_content_array.compact!
-  current_content_array.each do |character|
-    $tape.puts(character)
-  end
+@tape.rewind
+#How do we fix this?
+if content.empty?
+current_content_array = @tape.read.split("\n")
+else
+current_content_array = content
+end
+@tape.rewind
+current_content_array[char] = data
+current_content_array.compact!
+current_content_array.each do |character|
+@tape.puts(character)
+end
 end
 
 def tape_insert(name)
-  current_content = []
-  if File.exists?(name+".tap")
-    current_content = File.read(name+".tap").split("\n")
-  end
-
-  $tape = File.open(name+".tap","w+")
-  return current_content
+current_content = []
+if File.exists?(name+".tap")
+current_content = File.read(name+".tap").split("\n")
+end
+@tape = File.open(name+".tap","w+")
+return current_content
 end
 
 def tape_eject()
-  $tape.close()
-  $tape=nil
+@tape.close()
+@tape=nil
 end
 
-i=0
-content = tape_insert("test")
-tape_write(i,"a",content)
-i=1
-tape_write(i,"~",content)
-tape_eject()
-content = tape_insert("test")
-tape_write(i,"t",content)
-i=2
-tape_write(i,"~",content)
-tape_eject
+def endc(m=false,data=nil)
+if m
+@endc=data
+return
+else
+return endc
+end
+end
+
+
+end
