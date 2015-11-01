c260f0b530f66e184f61e093628864db6c791f98
Donovan Lampa <donovan.lampa@gmail.com>
2015-11-01 13:14:25 -0600

    Update formatting and add accessor

--- comp.rb
+++ comp.rb
@@ -1,54 +1,49 @@
 class Tape
-#reading endc:endc()
-#writing endc:endc(true,data)
-def initialize(end_char="~")
-@tape=""
-@endc
-end
+  #reading endc:class.endc
+  #writing endc:class.endc=data
 
-def tape_read(char)
-@tape.rewind()
-return @tape.read.split("\n")[char]
-end
+  attr_accesor :endc
 
-def tape_write(char,data,content=[])
-@tape.rewind
-#How do we fix this?
-if content.empty?
-current_content_array = @tape.read.split("\n")
-else
-current_content_array = content
-end
-@tape.rewind
-current_content_array[char] = data
-current_content_array.compact!
-current_content_array.each do |character|
-@tape.puts(character)
-end
-end
+  def initialize(end_char="~")
+    @tape=""
+    @endc=end_char
+  end
 
-def tape_insert(name)
-current_content = []
-if File.exists?(name+".tap")
-current_content = File.read(name+".tap").split("\n")
-end
-@tape = File.open(name+".tap","w+")
-return current_content
-end
+  def tape_read(char)
+    @tape.rewind()
+    return @tape.read.split("\n")[char]
+  end
 
-def tape_eject()
-@tape.close()
-@tape=nil
-end
+  def tape_write(char,data,content=[])
+    @tape.rewind
+    #How do we fix this?
+    if content.empty?
+      current_content_array = @tape.read.split("\n")
+    else
+      current_content_array = content
+    end
+    @tape.rewind
+    current_content_array[char] = data
+    current_content_array.compact!
+    current_content_array.each do |character|
+      @tape.puts(character)
+    end
+  end
 
-def endc(m=false,data=nil)
-if m
-@endc=data
-return
-else
-return endc
-end
-end
+  def tape_insert(name)
+    current_content = []
+    if File.exists?(name+".tap")
+      current_content = File.read(name+".tap").split("\n")
+    end
+    @tape = File.open(name+".tap","w+")
+    return current_content
+  end
+
+  def tape_eject()
+    @tape.close()
+    @tape=nil
+  end
 
 
 end
+
