855ce5b8961267d4fd51cf36ecba8fadf2dc3bdc
pjht <peterterpstra@Gifteds-MacBook-Pro.local>
2016-06-30 15:44:36 -0500

    Stuff

--- web server/server.js
+++ web server/server.js
@@ -1,30 +1,83 @@
+const debug=true
 const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
 const fs = require('fs');
-var index = fs.readFileSync('index.html');
-var css = fs.readFileSync('site.css');
+const index = fs.readFileSync('index.html');
+const blog = fs.readFileSync('Blog.html');
+const books = fs.readFileSync('Books.html');
+const css = fs.readFileSync('site.css');
+const pimg = fs.readFileSync('Pictures.jpg');
+const vimg = fs.readFileSync('Videos.jpg');
+const blimg = fs.readFileSync('Blog.jpg');
+const boimg = fs.readFileSync('Books.jpg');
+const himg = fs.readFileSync('Home.jpg');
 http.createServer(function (request, response) {
   var method = request.method;
   var url = request.url;
   var headers = request.headers;
   var accept = headers['accept'];
-  console.log(method);
-  console.log(url);
-  console.log(accept);
-  if (accept=='text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8') {
+  if (debug) {
+    console.log(method);
+    console.log(url);
+    console.log(accept);
+  }
+
+  if (url=='/' || url=='/index.html') {
     response.writeHead(200, {
       'Content-Type': 'text/html',
     });
     response.end(index);
-  } else if (accept=='text/css,*/*;q=0.1') {
+    console.log("200 OK: "+url);
+  } else if (url=='/Blog.html' ) {
+    response.writeHead(200, {
+      'Content-Type': 'text/html',
+    });
+    response.end(blog);
+    console.log("200 OK: "+url);
+   } else if (url=='/Books.html' ) {
+     response.writeHead(200, {
+       'Content-Type': 'text/html',
+     });
+     response.end(books);
+    console.log("200 OK: "+url);
+  } else if (url=='/css/site.css') {
     response.writeHead(200, {
       'Content-Type': 'text/css',
     });
     response.end(css);
+    console.log("200 OK: "+url);
+  } else if (url=='/pics/Pictures.jpg') {
+    response.writeHead(200, {
+      'Content-Type': 'image/jpg',
+    });
+    response.end(pimg);
+    console.log("200 OK: "+url);
+  } else if (url=='/pics/Videos.jpg') {
+      response.writeHead(200, {
+        'Content-Type': 'image/jpg',
+      });
+      response.end(vimg);
+      console.log("200 OK: "+url);
+  } else if (url=='/pics/Blog.jpg') {
+      response.writeHead(200, {
+        'Content-Type': 'image/jpg',
+      });
+      response.end(blimg);
+      console.log("200 OK: "+url); 
+  } else if (url=='/pics/Books.jpg') {
+      response.writeHead(200, {
+        'Content-Type': 'image/jpg',
+      });
+      response.end(boimg);
+      console.log("200 OK: "+url);
+   } else if (url=='/pics/Home.jpg') {
+       response.writeHead(200, {
+         'Content-Type': 'image/jpg',
+       });
+       response.end(himg);
+       console.log("200 OK: "+url); 
   } else {
     response.writeHead(404);
-    response.end();
+    response.end('<html><head><title>404</title></head><body><h1>404</h1></html>');
+    console.log("404 File not found: "+url); 
   }
-}).listen(port);
+}).listen(3000);
