4f13151d2b00a29fc237be2931a648b38211dbb5
pjht <peterterpstra@Gifteds-MacBook-Pro.local>
2016-12-14 15:02:48 -0600

    stuff

--- MEFSParse.rb
+++ MEFSParse.rb
@@ -1,5 +1,6 @@
-puts "MEFS sector:"
-sector=gets.chomp!
+file=File.open("disk.medsk","r")
+sector=file.gets.chomp!
+puts sector.inspect
 (512-sector.length).times do
   sector+=" "
 end
@@ -16,7 +17,10 @@ sector.each_char do |b|
       unless b==" "
        ext+=b
       end
+    when 37..61
+     puts "Sector Id byte:#{b.to_i(16)}"
   end
   i+=1
 end
+file.close
 puts filename+"."+ext
